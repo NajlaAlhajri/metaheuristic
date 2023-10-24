ad1eb87bd66e981bcc490abf55c96971a625e861
NajlaAlhajri <114076076+NajlaAlhajri@users.noreply.github.com>
2023-10-24 23:11:09 +0300

    Delete interactive-mo-ml-main directory

--- interactive-mo-ml-main/scripts/wrapper_experiments.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-[ "$(ls -A /home/interactive-mo-ml)" ] || cp -R /home/dump/. /home/interactive-mo-ml
-cd /home/interactive-mo-ml
-chmod 777 ./scripts/*
-
-python src/experiments_launcher.py
